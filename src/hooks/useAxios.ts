136c5cfe59bbca20e50d8dd025bc2926aa94ee8e
jeremiahtenbrink <jeremiahtenbrink@gmail.com>
2019-07-13 22:37:31 -0600

    Fixed useAxios.

--- src/hooks/useAxios.ts
+++ src/hooks/useAxios.ts
@@ -1,114 +1,117 @@
-import { useEffect, useState } from "react";
+import { Dispatch, SetStateAction, useEffect, useState, } from "react";
 import AxiosConfig from "./axiosConfig";
-import { AxiosInstance, AxiosRequestConfig } from "axios";
+import axios, { AxiosInstance } from 'axios';
+import { AxiosRequestConfig, AxiosResponse, AxiosError } from "axios";
 
 export const useAxios = ( config: AxiosRequestConfig | null = null ): [ IRequest, any, string, boolean ] => {
   
-  const [ value, setValue ]: [ any, ( any ) => void ] = useState( null );
-  const [ error, setError ]: [ string, ( string ) => void ] = useState( "" );
+  const [ value, setValue ]: [ any, ( value: any ) => void ] = useState( null );
+  const [ error, setError ]: [ string, ( error: string ) => void ] = useState(
+    "" );
   
-  const [ isLoading, setIsLoading ]: [ boolean, ( boolean ) => void ] = useState(
+  const [ isLoading, setIsLoading ]: [ boolean, Dispatch<SetStateAction<boolean>> ] = useState<boolean>(
     false );
   
-  const [ axiosRequest, setAxiosRequest ]: [ null | AxiosInstance, ( AxiosInstance ) => void ] = useState(
-    null );
-  
-  const [ axiosAuthRequest, setAxiosAuthRequest ]: [ null | AxiosInstance, ( AxiosInstance ) => void ] = useState(
-    null );
-  
-  useEffect( () => {
-    if ( config ) {
-      setAxiosRequest( AxiosConfig.createAxiosRequest( config ) );
-    } else {
-      setAxiosRequest( AxiosConfig.getDefaultAxios() );
-    }
-    setAxiosAuthRequest( AxiosConfig.getAuthAxios() );
-  }, [] );
+  const axiosRequest = config && AxiosConfig.createAxiosRequest( config );
+  const defaultRequest = !config;
   
   const get = ( url: string, auth: boolean = false ): void => {
-    
-    let request = axiosRequest;
+    debugger;
+    let request = AxiosConfig.getDefaultAxios();
     if ( auth ) {
-      request = axiosAuthRequest;
+      request = AxiosConfig.getAuthAxios();
+    } else if ( !defaultRequest ) {
+      request = axiosRequest as AxiosInstance;
     }
     
-    setIsLoading( true );
-    request.get( url ).then( res => {
-      setValue( res.data );
-      setIsLoading( false );
-      setError( "" );
-    } ).catch( err => {
-      setError( err.message );
-      setIsLoading( false );
-    } );
+    if ( request ) {
+      setIsLoading( true );
+      request.get( url ).then( ( res: AxiosResponse ) => {
+        setValue( res.data );
+        setIsLoading( false );
+        setError( "" );
+      } ).catch( ( err: AxiosError ) => {
+        setError( err.message );
+        setIsLoading( false );
+      } );
+    }
   };
   
   const post = ( url: string, data: any, auth: boolean = false ): void => {
     
-    let request = axiosRequest;
+    let request = AxiosConfig.getDefaultAxios();
     if ( auth ) {
-      request = axiosAuthRequest;
+      request = AxiosConfig.getAuthAxios();
+    } else if ( !defaultRequest ) {
+      request = axiosRequest as AxiosInstance;
     }
     
     setIsLoading( true );
-    request.post( url, data ).then( res => {
-      setValue( res.data );
-    } ).catch( err => {
-      setError( err.message );
-    } ).finally( () => setIsLoading( false ) );
+    if ( request ) {
+      request.post( url, data ).then( res => {
+        setValue( res.data );
+      } ).catch( err => {
+        setError( err.message );
+      } ).finally( () => setIsLoading( false ) );
+    }
   };
   
   const put = ( url: string, data: any, auth: boolean = false ): void => {
     
-    let request = axiosRequest;
+    let request = AxiosConfig.getDefaultAxios();
     if ( auth ) {
-      request = axiosAuthRequest;
+      request = AxiosConfig.getAuthAxios();
+    } else if ( !defaultRequest ) {
+      request = axiosRequest as AxiosInstance;
     }
     
     setIsLoading( true );
-    request.put( url, data ).then( res => {
-      setValue( res.data );
-    } ).catch( err => {
-      setError( err.message );
-    } ).finally( () => setIsLoading( false ) );
+    if ( request ) {
+      request.put( url, data ).then( res => {
+        setValue( res.data );
+      } ).catch( err => {
+        setError( err.message );
+      } ).finally( () => setIsLoading( false ) );
+    }
   };
   
   const del = ( url: string, object: any, auth: boolean = false ): void => {
     
-    let request = axiosRequest;
+    let request = AxiosConfig.getDefaultAxios();
     if ( auth ) {
-      request = axiosAuthRequest;
+      request = AxiosConfig.getAuthAxios();
+    } else if ( !defaultRequest ) {
+      request = axiosRequest as AxiosInstance;
     }
     
     setIsLoading( true );
-    request.delete( url, object ).then( res => {
-      setValue( res.data );
-    } ).catch( err => {
-      setError( err.message );
-    } ).finally( () => setIsLoading( false ) );
-  };
-  
-  const setDefaultConfig = ( config: AxiosRequestConfig ): void => {
-    AxiosConfig.setDefaultConfig( config );
-    setAxiosRequest( AxiosConfig.getDefaultAxios() );
-  };
-  
-  const setAuthConfig = ( config: AxiosRequestConfig ): void => {
-    AxiosConfig.setAuthConfig( config );
-    setAxiosAuthRequest( AxiosConfig.getDefaultAxios() );
+    if ( request ) {
+      request.delete( url, object ).then( res => {
+        setValue( res.data );
+      } ).catch( err => {
+        setError( err.message );
+      } ).finally( () => setIsLoading( false ) );
+    }
   };
   
   return [
-    { get, post, put, del, setDefaultConfig, setAuthConfig }, value, error,
+    { get, post, put, del }, value, error,
     isLoading
   ];
 };
 
+export const setDefaultConfig = ( config: AxiosConfig ): void => {
+  debugger;
+  AxiosConfig.setDefaultConfig( config );
+};
+
+export const setAuthConfig = ( config: AxiosConfig ): void => {
+  AxiosConfig.setAuthConfig( config );
+};
+
 interface IRequest {
-  get: ( string, boolean? ) => void;
-  post: ( string, any, boolean? ) => void;
-  put: ( string, any, boolean? ) => void;
-  del: ( string, any, boolean? ) => void;
-  setDefaultConfig: ( AxiosRequestConfig ) => void;
-  setAuthConfig: ( AxiosRequestConfig ) => void;
+  get: ( url: string, useAuth?: boolean ) => void;
+  post: ( url: string, useAuth?: boolean ) => void;
+  put: ( url: string, useAuth?: boolean ) => void;
+  del: ( url: string, useAuth?: boolean ) => void;
 }
